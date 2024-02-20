9402ad78f6b6a6593a16eb0ac90dd3fd57301b43
Yostena lewis <yostenalewis1>
2024-02-20 16:53:29 +0200

    Custom Model Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react"
+import Modal from "./UI/Modal"
 import ProductCard from "./components/ProductCard"
 import { productList } from "./data/data"
+import Button from "./UI/Button"
+
 
 const App = () => {
+  ////---------------state----------------
+  const [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  } 
   //render 
  const renderProductList = productList.map((product)=>(
     <ProductCard key={product.id} product={product} />
@@ -9,10 +23,19 @@ const App = () => {
 
   return (
     <main className="container" > 
-      <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md ">
-     {renderProductList }
+
+       <Button className="bg-green-500 hover:bg-green-600 " onClick={openModal}>ADD to card</Button>
+       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4 p-2 rounded-md ">
+       {renderProductList }
       </div>
-    </main>
+      <Modal isOpen={isOpen} closeModal={closeModal}  title="Add A NEW PRODUCT">
+         <div className="flex items-center justify-between space-x-2 mt-3"> 
+        <Button  className="bg-indigo-600 ">Submit</Button>
+        <Button className= "bg-gray-400 hover:bg-red-700 ">Cancel</Button>
+         </div>
+        </Modal>
+
+     </main>
   )
 }
 
